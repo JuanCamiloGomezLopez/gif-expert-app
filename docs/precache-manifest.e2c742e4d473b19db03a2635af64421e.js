ce9e974b21bca771ac18ebaf562f440a5d733fd2
camilo gomez <juancamilogomezl@gmail.com>
2020-08-27 21:16:23 -0500

    segundo commit

---
+++ docs/precache-manifest.e2c742e4d473b19db03a2635af64421e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba092e890c81b8c02d9bc43f07b4639",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cce75d8b504928b81380",
+    "url": "/static/css/main.1c2f082e.chunk.css"
+  },
+  {
+    "revision": "ddfc370ede98d9e59777",
+    "url": "/static/js/2.75f7596a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.75f7596a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cce75d8b504928b81380",
+    "url": "/static/js/main.8355df26.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
